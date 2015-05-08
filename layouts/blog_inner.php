8e9287317e33f2bba828e5267ac7785c65e9be47
Peter Ivanov <peter@microweber.com>
2015-05-08 18:17:46 +0300

    update

--- layouts/blog_inner.php
+++ layouts/blog_inner.php
@@ -7,12 +7,19 @@
         <div class="col-lg-9" id="blog-main-inner">
           <h3 class="page-title edit" field="title" rel="content">Page Title</h3>
           <div class="post-content">
-            <module data-type="pictures" data-template="simple" rel="content"  />
-            <div class="edit" field="content_body" rel="content">
-              <div class="element">
-                <p align="justify">This text is set by default and is suitable for edit in real time. By default the drag and drop core feature will allow you to position it anywhere on the site. Get creative, Make Web.</p>
-              </div>
+            
+            <div class="edit" field="content" rel="content">
+            
+                <module data-type="pictures" data-template="simple" rel="content"  />
+                <div class="edit" field="content_body" rel="content">
+                  <div class="element">
+                    <p align="justify">This text is set by default and is suitable for edit in real time. By default the drag and drop core feature will allow you to position it anywhere on the site. Get creative, Make Web.</p>
+                  </div>
+                </div>
+                
             </div>
+            
+            
           </div>
           <hr />
           <div >
