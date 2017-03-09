1a614017c4ca4341516c52243805ea08c77cee8b
Peter Ivanov <peter@microweber.com>
2017-03-09 14:51:45 +0200

    updates

--- footer.php
+++ footer.php
@@ -4,11 +4,6 @@
       <div class="mw-ui-col" id="copyright">
         <?php print powered_by_link(); ?>
       </div>
-      
-      <div class="mw-ui-col">
-        <div class="edit" field="darock_middle_footer" rel="global"></div>
-      </div>
-      
       <div class="mw-ui-col" id="powered">
         <a href="<?php print site_url(); ?>admin" class="admin-login">Admin Login</a>
       </div>
