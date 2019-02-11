a11f30b737cd5ccc4efbd80a3a8f0a0081e50f56
Petko Yovchevski <pyovchevski@gmail.com>
2019-02-11 14:38:06 +0200

    updates

--- modules/layouts/templates/skin-1.php
+++ modules/layouts/templates/skin-1.php
@@ -12,7 +12,18 @@ position: 0
 
 ?>
 
-<div class="nodrop clean-container edit" field="layout-skin-1-<?php print $params['id'] ?>" rel="module">
+<?php
+if (!$classes['padding_top']) {
+    $classes['padding_top'] = '';
+}
+if (!$classes['padding_bottom']) {
+    $classes['padding_bottom'] = '';
+}
+
+$layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom'] . ' ';
+?>
+
+<div class="nodrop clean-container edit <?php print $layout_classes; ?>" field="layout-skin-1-<?php print $params['id'] ?>" rel="module">
     <div class="container">
         <div class="box-container">
             <div class="allow-drop">
