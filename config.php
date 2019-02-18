e4598cad96e42e21bdae6431fea18254bdf37731
Peter Ivanov <peter@microweber.com>
2019-02-18 15:39:28 +0200

    update

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 $config = array();
 $config['name'] = "Da Rock";
 $config['author'] = "Microweber";
-$config['version'] = 1.1;
+$config['version'] = 1.2;
 $config['is_default'] = 1; //if you set this parameter the template will be selected on the install screen
 
